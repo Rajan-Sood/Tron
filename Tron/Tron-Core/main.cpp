2e47605cf1db5ab872cffe3630d96e0e3a81a497
Rajan-Sood <Rajan-Sood@users.noreply.github.com>
2019-05-11 11:17:05 -0400

    shaders

--- Tron/Tron-Core/main.cpp
+++ Tron/Tron-Core/main.cpp
@@ -1,5 +1,7 @@
-#include"source/graphics/window.h"
+ #include"source/graphics/window.h"
 #include"source/maths/maths.h"
+#include"source/utility/file_utility.h"
+#include"source/graphics/shader.h"
 
 int main()
 {
@@ -7,30 +9,56 @@ int main()
 	using namespace graphics;
 	using namespace maths;
 
+	
 	Window window("Tron!", 800, 600);
 
-	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-	vec3 a(1.0f, 2.0f,5.0f);
-	vec3 b(2, 4,6);
 
-	vec3 c = a + b;
+	GLfloat vertices[] = 
+	{
+		0,0,0,
+		8,0,0,
+		0,3, 0,
+		0,3, 0,
+		8,3,0,
+		8,0,0
+		
 
-	mat4 position = mat4::translation(vec3(2, 3, 4));
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0);
+	glEnableVertexAttribArray(0);
+
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+	
+	Shader shader("source/shaders/basic.vert", "source/shaders/basic.frag");
+	shader.enable();
+
+	shader.setUniformMat4("pr_matrix", ortho);
+	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4,3,0)));
+	
+	shader.setUniform2f("light_pos", vec2(8.0f, 4.5f));
+	shader.setUniform4f("colour", vec4(0.2f, 0.3f, 0.8f, 1.0f));
+
 
 	while (!window.closed())
 	{
+		/*
+		-0.5f, -0.5f , 0.0f,
+		-0.5f,  0.5f , 0.0f,
+		 0.5f,  0.5f , 0.0f,
+		 0.5f,  0.5f , 0.0f,
+		 0.5f, -0.5f , 0.0f,
+		-0.5f, -0.5f , 0.0f
+		*/
+		window.clear();
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		
-
-		std::cout << a << std::endl;
-		
-		glBegin(GL_QUADS);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(-0.5f, 0.5f);
-		glVertex2f(0.5f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-
-		glEnd();
 		window.update();
 	}
 
